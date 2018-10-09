82e145185f49ca5eca5b7aa8c3e32e86030ca7d3
Adrian Kujawski <adrian.kujawski.313@gmail.com>
2018-10-09 21:24:20 +0200

    count connections

--- SignalRchatAPI/SignalRchat/Hubs/ChatHub.cs
+++ SignalRchatAPI/SignalRchat/Hubs/ChatHub.cs
@@ -3,6 +3,8 @@ using MongoDB.Driver;
 using SignalRchat.Models;
 using SignalRchat.Services;
 using SignalRchat.Services.DAO;
+using SignalRchat.Services.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,6 +18,20 @@ namespace SignalRchat.Hubs
         public ChatHub(IMongoClient mongo) : base()
         {
             _mongo = mongo;
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            UserHandler.ConnectedIds.Add(Context.ConnectionId);
+            Clients.All.SendAsync("currentConnections", UserHandler.ConnectedIds.Count());
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            UserHandler.ConnectedIds.Remove(Context.ConnectionId);
+            Clients.All.SendAsync("currentConnections", UserHandler.ConnectedIds.Count());
+            return base.OnDisconnectedAsync(exception);
         }
 
         public void Send(string name, string message)
---
+++ SignalRchatAPI/SignalRchat/Services/Helpers/UserHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRchat.Services.Helpers
+{
+    public static class UserHandler
+    {
+        public static HashSet<string> ConnectedIds = new HashSet<string>();
+    }
+}
